681d850324ea563767bcb9e0397bb79a946697d5
Abdulrahuman <132744501+AbdulRahumankm@users.noreply.github.com>
2024-07-16 10:56:40 +0530

    Restarted

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 import { preview } from "../assets";
-import { getRendomPrompt } from "../utils";
+/*  import { getRendomPrompt } from "../utils"; */
 import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
@@ -15,7 +14,19 @@ const CreatePost = () => {
   const [genertingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  return <div>CreatePost</div>;
+  return (
+    <section className="max-w-7xl">
+      <div>
+        <h1 className="font-extrabold text-[#222328]">
+          Create
+        </h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
+          Create imaginative and visually stunning
+          images through DALL-E AI and share them with the Community
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default CreatePost;
