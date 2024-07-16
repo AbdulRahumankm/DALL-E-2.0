802c8372663e94c92021dbf70fab7baca84f3a40
Abdulrahuman <132744501+AbdulRahumankm@users.noreply.github.com>
2024-07-16 14:50:01 +0530

    server init

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+import postRoutes from './routes/postRoutes.js'
+import delleRoutes from './routes/delleRoutes.js'
+
+import connectDB from "./mongoDB/connect.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/", async (req, res) => {
+  res.send("Hello from DELL-E");
+});
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGO_URL);
+    app.listen(8080, () =>
+      console.log("Server has started on port http://localhost:8080")
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
